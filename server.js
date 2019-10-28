1c0e11797eda75b51ddb5ec8122775f03be7dd8d
Felipe Vasconcellos <vasconcellosdefreitas@outlook.com>
2019-10-28 01:10:27 -0300

    Ajustando rota server.js

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/meu-portifolio-felipe-vasconcellos'));
+app.use(express.static(`$/dist/$`));
 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(__dirname + '/index.html');
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`$/dist/$/index.html`));
 });
-// Start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
