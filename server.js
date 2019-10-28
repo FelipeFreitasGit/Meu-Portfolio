b9a684f0af87e87018c164f63e475f3a832001fb
Felipe Vasconcellos <vasconcellosdefreitas@outlook.com>
2019-10-28 01:34:26 -0300

    Ajustando rota server.js send

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 app.listen(process.env.PORT || 8080);
