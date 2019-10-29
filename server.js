237574254356c0035b84cdab9bc73acc2ad612fe
Felipe Vasconcellos <vasconcellosdefreitas@outlook.com>
2019-10-28 23:26:12 -0300

    package atualizado

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+//Importa as dependências que acabamos de instalar
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/meu-portifolio-felipe-vasconcellos'));
 
-app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/meu-portifolio-felipe-vasconcellos/index.html'));
 });
 
-app.listen(process.env.PORT || 4200);
+// Inicia a aplicação pela porta configurada
+app.listen(process.env.PORT || 8080);
