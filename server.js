38fc0e96d4694c67b5872472639ce5982e544988
Felipe Vasconcellos <vasconcellosdefreitas@outlook.com>
2019-11-22 01:12:19 -0200

    Ajustando configuraçoes heroku

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Importa as dependências que acabamos de instalar
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/felipe-meu-portifolio'));
+app.use(express.static(__dirname + '/dist/MyPortfolio'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/felipe-meu-portifolio/index.html'));
+res.sendFile(path.join(__dirname + '/dist/MyPortfolio/index.html'));
 });
 
-// Inicia a aplicação pela porta configurada
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
